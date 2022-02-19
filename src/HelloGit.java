ed2b7fc3852921149b13e22b5d3fe5ee8c2ae425
anisakalonova <akalonova@inths.org>
2022-02-19 13:46:11 -0600

    AnisasGithub

--- src/HelloGit.java
+++ src/HelloGit.java
@@ -2,5 +2,6 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Today is a beautiful day!");
         System.out.println("Hello!, Today is Saturday!");
+        System.out.println("I like hiking!");
     }
 }
